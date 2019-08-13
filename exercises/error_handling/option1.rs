9d272ad8ab2edc4337a2b0ac85dfbef92d062f54
kesnar <kesnar@gmail.com>
2019-08-13 15:41:44 +0300

    Solved error_handling

--- exercises/error_handling/option1.rs
+++ exercises/error_handling/option1.rs
@@ -7,10 +7,16 @@
 pub fn pop_too_much() -> bool {
     let mut list = vec![3];
 
-    let last = list.pop().unwrap();
+    let last = match list.pop() {
+        Some(ret) => ret,
+        None => return true,
+    };
     println!("The last item in the list is {:?}", last);
 
-    let second_to_last = list.pop().unwrap();
+    let second_to_last = match list.pop() {
+        Some(ret) => ret,
+        None => return true,
+    };
     println!(
         "The second-to-last item in the list is {:?}",
         second_to_last
